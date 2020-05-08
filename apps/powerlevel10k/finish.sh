da886e8e5a270da9ec741a07b7e503a5b056bd4f
Akash Kumar Dutta <akdboltzmann@gmail.com>
2020-05-08 16:14:29 +0530

    Add powerlevel10k instant prompt

--- apps/powerlevel10k/finish.sh
+++ apps/powerlevel10k/finish.sh
@@ -12,7 +12,22 @@ debug_info="\n"$(printf "=%.0s" {1..75})"\n ${BASH_SOURCE[0]} : "$(date)"\n""$(p
 echo -e $debug_info >> $LOG_FILE
 
 # Put powerlevel10k config to source on zshrc
-echo -e "\n#Config variables for custom apps installed via script">> $ZDOTDIR/.zshrc
-cat << EOF >> $ZDOTDIR/.zshrc
+p10k_config=$(cat << EOF
+#Config variables for custom apps installed via script
 [[ ! -f $POWERLEVEL10K_PATH/.p10k.zsh ]] || source $POWERLEVEL10K_PATH/.p10k.zsh
 EOF
+)
+
+instant_prompt=$(cat << 'EOF'
+# Enable Powerlevel10k instant prompt. Should stay close to the top of .zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block, everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+EOF
+)
+
+echo -e "$instant_prompt\n\n$(cat $ZDOTDIR/.zshrc)\n\n$p10k_config" > $ZDOTDIR/.zshrc
+
+
