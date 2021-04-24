176fe8e96737fbebafa9f6d386722bc92eb10309
Akash Kumar Dutta <akdboltzmann@gmail.com>
2021-04-24 16:35:59 +0530

    customize shapes

--- .config/polybar/shapes/scripts/updates.sh
+++ .config/polybar/shapes/scripts/updates.sh
@@ -39,7 +39,7 @@ while true; do
     # and network uptime, only checking once every 30 min for new updates
     while (( UPDATES == 0 )); do
         echo " None"
-        sleep 1800
+        sleep 3600
         get_total_updates
     done
 done
