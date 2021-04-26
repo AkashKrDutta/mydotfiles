a07ef662108302c2102ed3dc919cd1526727587a
Akash Kumar Dutta <akdboltzmann@gmail.com>
2021-04-27 02:16:15 +0530

    Package update notification works with dunst and dunst theme updated. Added 2 hour threshold to package notifications

--- .config/polybar/shapes/scripts/updates.sh
+++ .config/polybar/shapes/scripts/updates.sh
@@ -2,44 +2,64 @@
 
 NOTIFY_ICON=/usr/share/icons/Papirus-Dark/16x16/apps/system-software-update.svg
 
-get_total_updates() { UPDATES=$(checkupdates 2>/dev/null | wc -l); }
+get_total_updates() {
+    UPDATES=$(($(apt list --upgradable 2>/dev/null | wc -l) - 1))
+}
 
-while true; do
-    get_total_updates
-
-    # notify user of updates
-    if hash notify-send &>/dev/null; then
-        if (( UPDATES > 50 )); then
-            notify-send -u critical -i $NOTIFY_ICON \
-                "You really need to update!!" "$UPDATES New packages"
-        elif (( UPDATES > 25 )); then
-            notify-send -u normal -i $NOTIFY_ICON \
-                "You should update soon" "$UPDATES New packages"
-        elif (( UPDATES > 2 )); then
-            notify-send -u low -i $NOTIFY_ICON \
-                "$UPDATES New packages"
-        fi
+#while true; do
+#    get_total_updates
+#
+#    # notify user of updates
+#    if hash dunstify &>/dev/null; then
+#        if (( UPDATES > 50 )); then
+#            dunstify -h string:x-dunst-stack-tag:updates_count -u critical -i $NOTIFY_ICON \
+#                "You really need to update!!" "$UPDATES New packages"
+#            echo $UPDATES
+#        elif (( UPDATES > 0 )); then
+#            # Show update notification for 2 mins
+#            dunstify -h string:x-dunst-stack-tag:updates_count -u normal -t 120000 -i $NOTIFY_ICON \
+#                "$UPDATES New packages"
+#            echo $UPDATES
+#        fi
+#    fi
+#
+#    # when there are updates available
+#    # every 30 seconds another check for updates is done
+#    while (( UPDATES > 0 )); do
+#        sleep 30
+#        get_total_updates
+#        if (( UPDATES == 0 )); then
+#            echo "None"
+#        else
+#            echo "$UPDATES"
+#        fi
+#    done
+#
+#    #reached here means updates are 0 again
+#    #sleep for 4 hours to check again if upgraded or not
+#    echo  "None"
+#    #sleep $((3600*4))
+#    sleep 10
+#
+#done
+get_total_updates
+if (( UPDATES > 0 ));then
+    echo "$UPDATES"
+else
+    echo "None"
+fi
+threshHold = $((3600*2))
+. /home/akd/.config/polybar/shapes/scripts/lastUpdateNotify.sh 
+currTime=$(date +%s)
+if (( currTime-lastTime >= threshHold ));then
+    if (( UPDATES > 50 )); then
+        dunstify -h string:x-dunst-stack-tag:updates_count -u critical -i $NOTIFY_ICON \
+            "You really need to update!!" "$UPDATES New packages"
+        echo "lastTime="$currTime > /home/akd/.config/polybar/shapes/scripts/lastUpdateNotify.sh
+    elif (( UPDATES > 0 )); then
+        # Show update notification for 2 mins
+        dunstify -h string:x-dunst-stack-tag:updates_count -u normal -t 120000 -i $NOTIFY_ICON \
+            "$UPDATES New packages"
+        echo "lastTime="$currTime > /home/akd/.config/polybar/shapes/scripts/lastUpdateNotify.sh
     fi
-
-    # when there are updates available
-    # every 10 seconds another check for updates is done
-    while (( UPDATES > 0 )); do
-        if (( UPDATES == 1 )); then
-            echo " $UPDATES"
-        elif (( UPDATES > 1 )); then
-            echo " $UPDATES"
-        else
-            echo " None"
-        fi
-        sleep 10
-        get_total_updates
-    done
-
-    # when no updates are available, use a longer loop, this saves on CPU
-    # and network uptime, only checking once every 30 min for new updates
-    while (( UPDATES == 0 )); do
-        echo " None"
-        sleep 3600
-        get_total_updates
-    done
-done
+fi
